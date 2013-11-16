937e3d57d063a5380367e6bd9d2daf70c3c3ff48
Michael Lajlev <lajlev@gmail.com>
2013-11-16 23:00:54 +0100

    Top menu updated

--- sites/all/themes/uddannelsesuge/templates/partials/menu.tpl.php
+++ sites/all/themes/uddannelsesuge/templates/partials/menu.tpl.php
@@ -16,7 +16,8 @@
         <li><a href="/#activities">Aktiviteter</a></li>
         <li><a href="/#program">Program</a></li>
         <!-- <li><a href="/quiz">Uge 4 Quiz</a></li> -->
-        <li><a href="/om">Om uddannelsesugen</a></li>
+        <li><a href="/tilmeld">Tilmeld Klasse</a></li>
+        <li><a href="/om">Om Uddannelsesuge</a></li>
       </ul>
     </section>
   </nav>
