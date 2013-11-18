17e9acf8f725db604127d6a641680046ffda886b
Michael Lajlev <lajlev@gmail.com>
2013-11-18 14:22:58 +0100

    touch icons and favicon added

--- sites/all/themes/uddannelsesuge/templates/html.tpl.php
+++ sites/all/themes/uddannelsesuge/templates/html.tpl.php
@@ -51,6 +51,13 @@
 <head>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
+
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php print path_to_theme(); ?>/images/icons/apple-touch-icon-144x144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php print path_to_theme(); ?>/images/icons/apple-touch-icon-114x114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php print path_to_theme(); ?>/images/icons/apple-touch-icon-72x72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="<?php print path_to_theme(); ?>/images/icons/apple-touch-icon-57x57-precomposed.png">
+  <link rel="icon" type="image/png" href="<?php print path_to_theme(); ?>/images/icons/favicon.png">
+
   <meta name="viewport" content="width=device-width">
   <link href='http://fonts.googleapis.com/css?family=Raleway:300,600,800' rel='stylesheet' type='text/css'>
 
