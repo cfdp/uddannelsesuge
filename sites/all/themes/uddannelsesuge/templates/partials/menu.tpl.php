797599cd6e7754baa573bfb490a97edf0b9b80ab
Michael Lajlev <lajlev@gmail.com>
2013-11-22 17:27:55 +0100

    menu changed

--- sites/all/themes/uddannelsesuge/templates/partials/menu.tpl.php
+++ sites/all/themes/uddannelsesuge/templates/partials/menu.tpl.php
@@ -19,11 +19,10 @@
     <section class="top-bar-section">
       <!-- Right Nav Section -->
       <ul class="right">
-        <li><a href="/">Forside</a></li>
         <li><a href="/#activities">Aktiviteter</a></li>
         <li><a href="/#program">Program</a></li>
-        <!-- <li><a href="/quiz">Uge 4 Quiz</a></li> -->
         <li><a href="/tilmeld">Tilmeld Klasse</a></li>
+        <li><a href="/transport">Transport</a></li>
         <li><a href="/om">Om Uddannelsesuge</a></li>
       </ul>
     </section>
