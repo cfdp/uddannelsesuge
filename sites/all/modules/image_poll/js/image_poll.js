00adf041dbfbb20af36993a0139dbac1b20073c1
Michael Lajlev <lajlev@gmail.com>
2014-01-18 12:41:41 +0100

    faktaboks show if voted, outcomented

--- sites/all/modules/image_poll/js/image_poll.js
+++ sites/all/modules/image_poll/js/image_poll.js
@@ -1,18 +1,27 @@
 (function ($) {
     Drupal.behaviors.YourBehaviour = {
     attach: function() {
-
+        // Trigger radiobtn
         $('.poll.image_poll .choice').bind('touchstart mousedown', function(e){
             e.preventDefault();
             $(this).find('label').prop("checked", true).trigger("click");
         });
 
-        // Reveal extra info field after casting the vote for first time users
-        // $('[id^=image_poll-form]').click(function(event){
 
-        //     var mp_node = $(this).parents('.inner-wrapper');
-        //     mp_node.children('.extra-info-wrapper').slideDown();
-        // });
+        // Display faktaboks if poll has been voted
+        // var pollBar = $('.node-image-poll .poll-bar');
+
+        // if( pollBar.hasClass('voted') ) {
+        //     pollBar.closest('.inner-wrapper').find('.field-name-field-extra-info').show();
+        //     pollBar.closest('.inner-wrapper').find('.extra-info-wrapper').show();
+        // }
+
+        // Reveal extra info field after casting the vote for first time users
+        $('[id^=image_poll-form]').click(function(event){
+
+            var mp_node = $(this).parents('.inner-wrapper');
+            mp_node.children('.extra-info-wrapper').slideDown();
+        });
 
         // When changing votes, first the vote is canceled and then
         // submit the other choice programatically
